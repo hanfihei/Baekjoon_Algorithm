916860e2af12ab734cb9374655ff3f7dfd98018f
jihei <han01031@naver.com>
2026-01-13 22:37:55 +0900

    2026.01.13

---
+++ Card.java
@@ -0,0 +1,26 @@
+package 프로그래머스;
+
+class Card {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+
+        int c1 = 0;
+        int c2 = 0;
+
+        String answer = "Yes";
+
+        for(int i = 0; i<goal.length; i++){
+            if(c1<cards1.length && cards1[c1].equals(goal[i])){
+                c1++;
+                continue;
+            }
+            else if(c2<cards2.length && cards2[c2].equals(goal[i])){
+                c2++;
+                continue;
+            }
+            else
+                return "No";
+        }
+
+        return answer;
+    }
+}
